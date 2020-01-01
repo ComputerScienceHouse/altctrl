e3785cd60994ba964ec98bcef1d825767cb249af
willnilges <wdn5796@rit.edu>
2020-01-01 10:34:52 -0500

    Change a few things

--- src/protocol.rs
+++ src/protocol.rs
@@ -3,14 +3,24 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct NewWindow {
     pub id: String,
-    pub content: String,
+    pub content: String, //Contents?
     pub x_pos: i32,
     pub y_pos: i32,
     pub width: i32,
     pub height: i32,
 }
 
-// i2c stuff
+//Contents of window. Text? Progress bar? List? (Needs titles for stuff as well as text decoration such as bold, invert, and flashing)
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum WindowContent {
+    Text,
+    List,
+    Chart,
+    ProgressBar,
+}
+
+
+// Represents a port on a device in the system
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub enum Button {
     B1,
